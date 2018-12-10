1616225efb53dbba2d1d0745b119d3c4c2d49bba
Emilijus <slibinas2012@gmail.com>
2018-12-10 14:26:10 +0200

    Added quit button

---
+++ Assets/Quit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class Quit : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKey("escape"))
+            {
+                Debug.Log("quit requested");
+                Application.Quit();
+            }
+        }
+    } 
+}
